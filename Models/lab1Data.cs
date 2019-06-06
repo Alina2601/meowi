001b3e5389e5ad5297ff5601a3b2b90de792af1e
Alina2601 <malinak0906@gmail.com>
2019-06-06 19:49:49 +0700

    lab init

---
+++ Models/lab1Data.cs
@@ -0,0 +1,39 @@
+using pavlovLab.Models;
+
+namespace to.Models
+{
+    public class lab1Data
+    {
+        public string Product { get; set;}
+        public string Material { get; set;}
+        public int Content { get; set;}
+        public int Price { get; set;}
+    public BaseModelValidationResult Validate()
+        {
+            var validationResult = new BaseModelValidationResult();
+
+            if (string.IsNullOrWhiteSpace(Product))
+            
+                validationResult.Append($"Product cannot be empty");
+          
+            if (string.IsNullOrWhiteSpace(Material))
+            
+                validationResult.Append($"Material cannot be empty");
+            
+            if (!(84 < Content && Content < 10001)) 
+                validationResult.Append($"Content {Content} cannot be <85");
+           
+            if (!(0<Price))
+            
+                validationResult.Append($"Price cannot be <0");
+            
+              return validationResult;
+        }
+
+        public override string ToString()
+        {
+            return $"{Product} {Material} ptoba {Content} price {Price}";
+        }
+
+    }
+}
